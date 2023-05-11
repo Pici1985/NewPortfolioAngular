a1f1cd9d98ad2611dbad3fb43dacbe3a6bbceb18
Balazs Ferencz <ferenczbalazs85@gmail.com>
2023-05-11 19:39:10 +0100

    project details works ( unfinished )

--- src/app/Components/details/details.component.ts
+++ src/app/Components/details/details.component.ts
@@ -1,15 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProjectsService } from 'src/app/Services/projects.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent { 
-    
-    constructor() {
-      this.cardId = 0; // or any other initial value
-    }
+export class DetailsComponent {
+  project: any;
+  projectListSubscription: Subscription = new Subscription();
+  
+  constructor(private route: ActivatedRoute, private ProjectsService: ProjectsService,) { }    
 
-    @Input() cardId: number;
+  ngOnInit():void  {
+    this.route.params.subscribe((params) => {
+      const projectId = +params['id'];
+      this.projectListSubscription = this.ProjectsService.getProject(projectId).subscribe((data: any) => {
+        this.project = data;
+        console.log(data);
+      })
+    });
+  }
 }
