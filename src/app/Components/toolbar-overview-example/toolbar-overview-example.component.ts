a5cdecbf3eb756b679ab13e128eb5956c6143b90
Balazs Ferencz <ferenczbalazs85@gmail.com>
2023-05-12 13:17:23 +0100

    navigate buttons added ( unfinished )

--- src/app/Components/toolbar-overview-example/toolbar-overview-example.component.ts
+++ src/app/Components/toolbar-overview-example/toolbar-overview-example.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router, ActivatedRoute  } from '@angular/router';
 
 
 @Component({
@@ -7,10 +8,19 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./toolbar-overview-example.component.css']
 })
 export class ToolbarOverviewExampleComponent {
-  constructor() { }
+
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   @Output() toggleEvent = new EventEmitter();
  
+  goBack() : void {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+  goForward() : void {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
   toggle() {
     this.toggleEvent.emit();
   }
